350a9ed62bedfe2296fb5bae08dd434b35360a9e
yhvh1413 <61896829+yhvh1413@users.noreply.github.com>
2020-10-24 18:38:00 +0800

    Add files via upload

---
+++ wd.py
@@ -0,0 +1,131 @@
+#coding=utf-8
+import re
+import string
+import csv
+import jieba
+import jieba.analyse
+import jieba.posseg
+from collections import Counter
+
+
+def dosegment_all(sentence):
+    """
+    带词性标注，对句子进行分词，不排除停词等
+    :param sentence:输入字符
+    :return:
+    """
+    sentenceeged = jieba.posseg.cut(sentence.strip())
+    outstr = ""
+    for x in sentenceeged:
+        outstr += "{}/{},".format(x.word, x.flag)
+    # outstr = ",".join([("%s/%s" %(x.word,x.flag)) for x in sentenceeged])
+    print(outstr)
+
+def jieba_analysis(sent):
+    str_quan1 = jieba.cut(sent, cut_all=True)
+    print("全模式分词：{ %d}" % len(list(str_quan1)))
+    str_quan2 = jieba.cut(sent, cut_all=True)
+    print("/".join(str_quan2))
+    # print(str(str_1))  
+    # str_1_len=len(list(str_1))
+
+    str_jing1 = jieba.cut(sent, cut_all=False)
+    print("精准模式分词：{ %d}" % len(list(str_jing1)))
+    str_jing2 = jieba.cut(sent, cut_all=False)
+    print("/".join(str_jing2))
+
+    str_soso1 = jieba.cut_for_search(sent)
+    print('搜索引擎分词：{ %d}' % len(list(str_soso1)))
+    str_soso2 = jieba.cut_for_search(sent)
+    print("/".join(str_soso2))
+
+def jieba_count(sent):
+    text = sent
+    item = text.strip("\n\r").split("\t")
+    tags = jieba.analyse.extract_tags(item[0])
+
+    word_lst = []
+    for t in tags:
+        word_lst.append(t)
+
+    word_dict = {}
+    for item in word_lst:
+        if item not in word_dict:
+            word_dict[item] = 1
+        else:
+            word_dict[item] += 1
+    print(word_dict)
+
+def count_words(sent):
+    # 统计单词出现的次数
+    count = Counter(sent)
+    print(count)
+
+def jb(word):
+    s = word
+    sentence = re.sub("[%s]" % re.escape(
+        string.punctuation), "", s).replace(" ", "")
+    jieba_analysis(sentence)
+    count_words(sentence)
+    jieba_count(sentence)
+    dosegment_all(sentence)
+
+def removePunctuation(text):
+    text = re.sub(r"[{}]+".format(punctuation), "", text)
+    return text.strip().lower()
+
+def word_cut(sent, num): 
+    index = 0
+    while index < len(sent):
+        if index+num <= len(sent):
+            letter = sent[index:index+num]
+            index += 1
+            # print(letter)
+            for value in dict_all:
+                if letter == value["name"]:
+                    new.append(value.copy())
+        else:
+            break
+
+
+word_dict = "/Users/roon/Desktop/YHVH/Soda复赛demo/data/nlp_v.sql"
+
+print('请输入你的标签和词语')
+words = input("你现在想说: ")
+punctuation = "!,;:?\‘。，/"
+explain = removePunctuation(words)  
+
+new = [] 
+results = [] 
+
+fr = open(word_dict, "r")
+dict_all = eval(fr.read())
+fr.close()
+
+s = explain 
+sentence = re.sub("[%s]" % re.escape(
+    string.punctuation), "", s).replace(" ", "")
+
+word_cut(sentence, 1)
+word_cut(sentence, 2)
+word_cut(sentence, 3)
+word_cut(sentence, 4)
+word_cut(sentence, 5)
+
+n = sum(item["n"] for item in new)
+p = sum(item["p"] for item in new)
+nu1 = sum(item["nu1"] for item in new)
+nu2 = sum(item["nu2"] for item in new)
+nu3 = sum(item["nu3"] for item in new)
+nu4 = sum(item["nu4"] for item in new)
+nu5 = sum(item["nu5"] for item in new)
+nu6 = sum(item["nu6"] for item in new)
+f = sum(item["f"] for item in new)
+m = sum(item["m"] for item in new)
+
+new = [n, p, nu1, nu2, nu3, nu4, nu5, nu6, f, m]  # 把数值导入新csv文件
+new1 = [nu1, nu2, nu3, nu4, nu5, nu6]
+# print("\t%s\t%d\t%d\n\t\t%d\t%d\t%d\t%d\t%d\t%d\n\t\t%d\t%d" %
+#     ("sent:", n, p,  nu1, nu2, nu3, nu4, nu5, nu6, f, m))
+
+# jb(words)
---
+++ 时尚标签转化_main.py
@@ -0,0 +1,100 @@
+# coding=utf-8
+import wd
+
+
+def f_style(data):
+    print("穿衣风格分析开始......")
+    print("穿衣方式：常规-经典-流行-未来-独特")
+    st1 = {"常规": data[3], 
+        "经典": data[2]+data[3], 
+        "流行": data[1]+data[3], 
+        "未来": data[1]+data[2], 
+        "独特": data[0]+data[1]}
+    print("可能会喜欢：", max(st1, key=lambda i: st1[i]))
+
+    print('～'*50)
+    print("注重点：衣服-包-鞋子-首饰-眼镜-围巾/领带")
+    st2 = {"衣服": data[0]+data[3], 
+        "包": data[4], 
+        "鞋子": data[4]+data[5], 
+        "首饰": data[3]+data[4], 
+        "眼镜": data[2]+data[3], 
+        "围巾/领带": data[2]+data[5]}
+    print("可能会喜欢：", max(st2, key=lambda i: st2[i]))
+
+    print('～'*50)
+    print("关注点：发型-化妆-体型-健康-心态")
+    st3 = {"发型": data[1]+data[4], 
+        "化妆": data[0]+data[3], 
+        "体型": data[3]+data[4], 
+        "健康": data[0]+data[4], 
+        "心情": data[1]+data[2]}
+    print("可能会喜欢：", max(st3, key=lambda i: st3[i]))
+
+    print('-'*50)
+    print("款式：宽松-运动-休闲-商务-正装-礼服	")
+    st4 = {"宽松": data[0]+data[2], 
+        "运动": data[0]+data[5], 
+        "休闲": data[1]+data[5], 
+        "商务": data[3]+data[4], 
+        "正装": data[2]+data[3], 
+        "礼服": data[1]+data[3]}
+    print("可能会喜欢：", max(st4, key=lambda i: st4[i]))
+
+    print('～'*50)
+    print("材料：化学-金属-混合-天然")
+    st5 = {"化学": data[3]+data[5], 
+        "金属": data[3]+data[4], 
+        "混合": data[2]+data[3], 
+        "天然": data[1]+data[3]}
+    print("可能会喜欢：", max(st5, key=lambda i: st5[i]))
+
+    print('～'*50)
+    print("图案类型：字母-人物-花卉-几何-寓意")
+    st6 = {"字母": data[2], 
+        "人物": data[0], 
+        "花卉": data[1], 
+        "几何": data[2]+data[3], 
+        "寓意": data[1]+data[2]}
+    print("可能会喜欢：", max(st6, key=lambda i: st6[i]))
+
+    print('～'*50)
+    print("品牌需求：小众-独立-时尚-大众-个人喜好")
+    st7 = {"小众": data[0]+data[4], 
+        "独立": data[2], 
+        "时尚": data[1]+data[3], 
+        "大众": data[2]+data[3], 
+        "无个人喜好": data[0]}
+    print("可能会喜欢：", max(st7, key=lambda i: st7[i]))
+
+    print('～'*50)
+    print("线条类型：直-曲-波浪-折-弧-长-短-乱-规则")
+    st8 = {"直": data[0]+data[5], 
+        "曲": data[1], 
+        "波浪": data[1]+data[5], 
+        "折": data[2]+data[3], 
+        "弧": data[1]+data[2], 
+        "长": data[1]+data[4]+data[5],
+        "短": data[0]+data[2]+data[3],
+        "乱": data[1]+data[2], 
+        "规则": data[2]+data[3]}
+    print("可能会喜欢：", max(st8, key=lambda i: st8[i]))
+
+    print('～'*50)
+    print("几何类型：点-圆-椭圆-方-长方-三角-五角星-多角星-规则-不规则")
+    st9 = {"点": data[0], 
+        "圆": data[0]+data[1], 
+        "椭圆": data[0]+data[1]+data[2],
+        "方": data[2]+data[3], 
+        "长方": data[1]+data[2]+data[3],
+        "三角": data[2]+data[3]+data[4],
+        "五角星": data[3]+data[4], 
+        "多角星": data[4]+data[5], 
+        "规则": data[2]+data[3], 
+        "不规则": data[1]}
+    print("可能会喜欢：", max(st9, key=lambda i: st9[i]))
+
+
+if __name__ == "__main__":
+    print("机器开始分析", "."*100)
+    f_style(wd.new1)
